c8ad6d299a86f8b9bdd1b150aed0776c93a47bd3
robin <robintsunny@gmail.com>
2018-04-19 22:48:03 -0700

    Added Map class

--- W1D5/w1d5_hw.rb
+++ W1D5/w1d5_hw.rb
@@ -35,3 +35,59 @@ class Queue
   end
 
 end
+
+
+class Map
+  def initialize
+    @ivar = []
+  end
+
+
+  def assign(k,v)
+    if key_exists?(k)
+      @ivar.each do |el|
+        if el[0] == k
+          el[1] = v
+        end
+      end
+    else
+      @ivar << [k,v]
+    end
+    @ivar
+  end
+
+
+  def lookup(k)
+    return "no key exists" if key_exists?(k) == false
+
+    @ivar.each_with_index do |el,i|
+      return el[1] if el[0] == k
+    end
+    @ivar
+  end
+
+
+  def key_exists?(k)
+    @ivar.each do |el|
+      return true if el[0] == k
+    end
+    false
+  end
+
+
+  def remove(k)
+    return "no changes made" if key_exists?(k) == false
+
+    @ivar.each_with_index do |el,i|
+      @ivar.delete_at(i) if el[0] == k
+    end
+    @ivar
+  end
+
+
+
+  def show
+    @ivar
+  end
+
+end
