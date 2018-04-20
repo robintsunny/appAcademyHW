804cbfe228ae79f1bbfb121c3d94451ec8e8d21d
robin <robintsunny@gmail.com>
2018-04-19 22:47:30 -0700

    Added Queue class

--- W1D5/w1d5_hw.rb
+++ W1D5/w1d5_hw.rb
@@ -16,3 +16,22 @@ class Stack
     @ivar
   end
 end
+
+class Queue
+  def initialize
+    @ivar = []
+  end
+
+  def enqueue(el)
+    @ivar.push(el)
+  end
+
+  def dequeue
+    @ivar.shift
+  end
+
+  def show
+    @ivar
+  end
+
+end
